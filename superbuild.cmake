800d8361e7b01f6214e85068047d068c8d17587f
SmallIshMink34 <mattheo.m10@gmail.com>
2023-10-11 14:50:03 +0200

    Remove Superbuild pour l'installation du SDL2, car deja fait

--- superbuild.cmake
+++
@@ -1,5 +0,0 @@
-# Install SDL2 and SDL2_image
-execute_process(COMMAND sudo apt-get install -y libsdl2-dev libsdl2-image-dev)
-
-# Include the main CMakeLists.txt for your project
-add_subdirectory(${CMAKE_SOURCE_DIR})
